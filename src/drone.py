cad1d12f479d11612eafdf668418276fe718c18e
Thomas Mattelaer <mattelaer.thomas@hotmail.com>
2026-08-05 15:59:12 +0200

    drone class + first visualisation

---
+++ src/drone.py
@@ -0,0 +1,10 @@
+from typing import Optional
+class Drone ():
+
+    def __init__(self):
+        self.drone_id
+        self.current_hub
+        self.target_hub
+        self.in_transit
+
+    def 
--- src/map.py
+++ src/map.py
@@ -5,29 +5,31 @@ from parser import MapModel
 class MapVisualiser():
     def __init__(self,
                  map_data: MapModel,
-                 width: int = 800,
-                 height: int = 600
+                 width: int = 1280,
+                 height: int = 720
                  ) -> None:
         self.map_data = map_data
         self.width = width
         self.height = height
+        self.drone_img: pygame.Surface
 
     def run(self):
         pygame.init()
-        screen = pygame.display.set_mode((1280, 720))
+        screen = pygame.display.set_mode((self.width, self.height))
         clock = pygame.time.Clock()
         running = True
-        print(self.map_data.drones)
-        color = pygame.Color('lightskyblue3')
-
+        drone_img = pygame.image.load("./ressources/drone.png").convert_alpha()
+        self.drone_img = pygame.transform.scale(drone_img, (50, 50))
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("purple")
-            input_rect = pygame.Rect(200, 200, 140, 30)
-            pygame.draw.rect(screen, color, input_rect)
-            # RENDER YOUR GAME HERE
+            screen.fill("dark grey")
+            self.draw_drones(screen)
             pygame.display.flip()
             clock.tick(60)
         pygame.quit()
+
+    def draw_drones(self, screen: pygame.Surface) -> None:
+        for x in range(self.map_data.drones):
+            screen.blit(self.drone_img, (10 + (x * 10), self.height // 2))
